9ffb8f75831d2ecea0637b9a6464df92c03e1338
Anayeli Martinez Rosas <115113894+amartinezrosas@users.noreply.github.com>
2023-05-10 16:41:05 -0700

    Update pin_mux_config.c

--- Lab4_1/pin_mux_config.c
+++ Lab4_1/pin_mux_config.c
@@ -36,7 +36,7 @@
 //
 //*****************************************************************************
 
-// This file was automatically generated on 5/2/2023 at 5:07:11 PM
+// This file was automatically generated on 5/9/2023 at 3:02:33 PM
 // by TI PinMux version 4.0.1543 
 //
 //*****************************************************************************
@@ -61,27 +61,25 @@ void PinMuxConfig(void)
     PinModeSet(PIN_02, PIN_MODE_0);
     PinModeSet(PIN_03, PIN_MODE_0);
     PinModeSet(PIN_04, PIN_MODE_0);
-    PinModeSet(PIN_06, PIN_MODE_0);
+    PinModeSet(PIN_15, PIN_MODE_0);
     PinModeSet(PIN_21, PIN_MODE_0);
     PinModeSet(PIN_45, PIN_MODE_0);
-    PinModeSet(PIN_50, PIN_MODE_0);
     PinModeSet(PIN_52, PIN_MODE_0);
     PinModeSet(PIN_53, PIN_MODE_0);
     PinModeSet(PIN_60, PIN_MODE_0);
     PinModeSet(PIN_61, PIN_MODE_0);
-    PinModeSet(PIN_15, PIN_MODE_0);
+    PinModeSet(PIN_62, PIN_MODE_0);
     PinModeSet(PIN_63, PIN_MODE_0);
     
     //
     // Enable Peripheral Clocks 
     //
-    PRCMPeripheralClkEnable(PRCM_GPIOA0, PRCM_RUN_MODE_CLK);
     PRCMPeripheralClkEnable(PRCM_GPIOA1, PRCM_RUN_MODE_CLK);
     PRCMPeripheralClkEnable(PRCM_GPIOA2, PRCM_RUN_MODE_CLK);
     PRCMPeripheralClkEnable(PRCM_GPIOA3, PRCM_RUN_MODE_CLK);
     PRCMPeripheralClkEnable(PRCM_GSPI, PRCM_RUN_MODE_CLK);
-    PRCMPeripheralClkEnable(PRCM_UARTA1, PRCM_RUN_MODE_CLK);
     PRCMPeripheralClkEnable(PRCM_UARTA0, PRCM_RUN_MODE_CLK);
+    PRCMPeripheralClkEnable(PRCM_UARTA1, PRCM_RUN_MODE_CLK);
 
     //
     // Configure PIN_64 for GPIO Output
@@ -96,10 +94,10 @@ void PinMuxConfig(void)
     GPIODirModeSet(GPIOA2_BASE, 0x2, GPIO_DIR_MODE_OUT);
 
     //
-    // Configure PIN_62 for GPIO Input
+    // Configure PIN_16 for GPIO Output
     //
-    PinTypeGPIO(PIN_62, PIN_MODE_0, false);
-    GPIODirModeSet(GPIOA0_BASE, 0x80, GPIO_DIR_MODE_IN);
+    PinTypeGPIO(PIN_16, PIN_MODE_0, false);
+    GPIODirModeSet(GPIOA2_BASE, 0x80, GPIO_DIR_MODE_OUT);
 
     //
     // Configure PIN_18 for GPIO Output
@@ -108,24 +106,24 @@ void PinMuxConfig(void)
     GPIODirModeSet(GPIOA3_BASE, 0x10, GPIO_DIR_MODE_OUT);
 
     //
+    // Configure PIN_50 for SPI0 GSPI_CS
+    //
+    PinTypeSPI(PIN_50, PIN_MODE_9);
+
+    //
     // Configure PIN_05 for SPI0 GSPI_CLK
     //
     PinTypeSPI(PIN_05, PIN_MODE_7);
 
     //
+    // Configure PIN_06 for SPI0 GSPI_MISO
+    //
+    PinTypeSPI(PIN_06, PIN_MODE_7);
+
+    //
     // Configure PIN_07 for SPI0 GSPI_MOSI
     //
     PinTypeSPI(PIN_07, PIN_MODE_7);
-
-    //
-    // Configure PIN_58 for UART1 UART1_TX
-    //
-    PinTypeUART(PIN_58, PIN_MODE_6);
-
-    //
-    // Configure PIN_59 for UART1 UART1_RX
-    //
-    PinTypeUART(PIN_59, PIN_MODE_6);
 
     //
     // Configure PIN_55 for UART0 UART0_TX
@@ -136,4 +134,16 @@ void PinMuxConfig(void)
     // Configure PIN_57 for UART0 UART0_RX
     //
     PinTypeUART(PIN_57, PIN_MODE_3);
+
+    //
+    // Configure PIN_58 for UART1 UART1_TX
+    //
+    PinTypeUART(PIN_58, PIN_MODE_6);
+
+    //
+    // Configure PIN_59 for UART1 UART1_RX
+    //
+    PinTypeUART(PIN_59, PIN_MODE_6);
+}
+
 }
